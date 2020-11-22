f2b8016b3dd15f817204fdb74e4e0346d3420540
FrancescoManuguerra <manuguerra.fr@gmail.com>
2020-11-22 10:16:47 +0100

    service logger

--- RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/service/RegisterService.java
+++ RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/service/RegisterService.java
@@ -1,9 +1,13 @@
 package Elis.College.RegistroPranzi.service;
 
-
+import Elis.College.RegistroPranzi.controller.PrenotationController;
+import Elis.College.RegistroPranzi.exception.model.exceptionimpl.EntityNotFoundException;
 import Elis.College.RegistroPranzi.model.Register;
 import Elis.College.RegistroPranzi.repository.RegisterRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
@@ -15,26 +19,30 @@ public class RegisterService {
     @Autowired
     RegisterRepository registerRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(RegisterService.class);
 
     //TODO doesn't work
     //get prenotations by userId
-    public List<Register> getAllByUser_id(Long userId){
-        System.out.println("SERVICE : userId: "+userId);
-        return registerRepository.findAllByUser_Id(userId);
+    public List<Register> getAllByUser_id(Long userId, String logId){
 
+        List<Register> registerList = registerRepository.findAllByUser_Id(userId);
+        logger.info(logId+ " Got result from db");
+        if (registerList.isEmpty()) throw new EntityNotFoundException(HttpStatus.NOT_FOUND, 404,"Registration","No prenotation for this userId: "+userId );
+
+        return registerList ;
     }
 
     //get prenotations by date
-    public Integer countPrenotationsByDate(Date date){
-        System.out.println("SERVICE: date: "+date);
+    public Integer countPrenotationsByDate(Date date, String logId){
 
-        return registerRepository.countAllByDate(date);
+        Integer value = registerRepository.countAllByDate(date);
+        logger.info(logId+ "Got result from db");
+        return value;
     }
 
     //save prenotation
-    public void savePrenotation(Register register){
-        System.out.println("Service now before register 1 ");
-        System.out.println("REPOSITORY: GET DATE " + register.getDate());
+    public void savePrenotation(Register register,String logId){
+
         if(registerRepository.getAllByUser_idAndAndDate(register.getUser().getId(),register.getDate())==null) registerRepository.save(register);
         else {
             Register register1= registerRepository.getAllByUser_idAndAndDate(register.getUser().getId(),register.getDate());
@@ -45,5 +53,4 @@ public class RegisterService {
             registerRepository.save(register1);
         }
     }
-
 }
--- RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/service/UserService.java
+++ RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/service/UserService.java
@@ -1,10 +1,15 @@
 package Elis.College.RegistroPranzi.service;
 
+import Elis.College.RegistroPranzi.controller.PrenotationController;
 import Elis.College.RegistroPranzi.exception.model.exceptionimpl.EntityNotFoundException;
+import Elis.College.RegistroPranzi.exception.model.exceptionimpl.InternalServerErrorException;
 import Elis.College.RegistroPranzi.model.LoginKey;
+import Elis.College.RegistroPranzi.model.Register;
 import Elis.College.RegistroPranzi.model.User;
 import Elis.College.RegistroPranzi.repository.RegisterRepository;
 import Elis.College.RegistroPranzi.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -23,42 +28,63 @@ public class UserService {
     @Autowired
     RegisterRepository registerRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     //GET ALL USERS
-    public List<User> getUsers(){
-        return userRepository.findAll();
+    public List<User> getUsers(String logId) {
+
+        List<User> userList= userRepository.findAll();
+        logger.info(logId+ " Got result from db");
+        return userList;
     }
 
 
     //CHECK AUTHENTICATION
-    public User login(LoginKey loginKey) {
-        System.out.println("Service now, email: "+loginKey.getEmail()+" password: "+loginKey.getPassword());
-        User user= userRepository.getByEmailAndPassword(loginKey.getEmail(),loginKey.getPassword());
-         if(user  == null ) throw new EntityNotFoundException(HttpStatus.NOT_FOUND,404, "email or password","Wrong credential");
+    public User login(LoginKey loginKey, String logId) {
+
+        User user = userRepository.getByEmailAndPassword(loginKey.getEmail(), loginKey.getPassword());
+        logger.info(logId + " Got result from db");
+        if (user == null)
+            throw new EntityNotFoundException(HttpStatus.NOT_FOUND, 404, "Error", "The email or the password is wrong");
         return user;
     }
+
     //GET USER
-    public User getUser(Long id){
-        return userRepository.getById(id);
+    public User getUser(Long id, String logId) {
+
+        User user = userRepository.getById(id);
+        logger.info(logId + " Got result from db");
+        if (user == null)
+            throw new EntityNotFoundException(HttpStatus.NOT_FOUND, 404, "User", "User not found with id: " + id);
+
+        return user;
     }
 
     //SAVE USER
-    public User saveUser(User user){
+    public User saveUser(User user, String logId) {
+
         userRepository.save(user);
-        User checkuser= userRepository.getByEmailAndPassword(user.getEmail(),user.getPassword());
-        if(checkuser!=null)return checkuser;
-        return null;
-
+        logger.info(logId + " Post repo done");
+        User checkuser = userRepository.getByEmailAndPassword(user.getEmail(), user.getPassword());
+        logger.info(logId + " Got result from db");
+        if (checkuser == null)
+            throw new InternalServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, 500, "User", "The server failed to perform the operation");
+        return checkuser;
     }
+
     //DELETE USER
-    public void deleteUserById(Long id){
+    public void deleteUserById(Long id, String logId) {
 
-            registerRepository.deleteAllByUser_Id(id);
-            userRepository.deleteById(id);
+        registerRepository.deleteAllByUser_Id(id);
+        logger.info(logId + " Delete register repo done");
+        userRepository.deleteById(id);
+        logger.info(logId + " Delete user repo done");
 
+        List<Register> registerList = registerRepository.getAllByUser_id(id);
+        User user = userRepository.getById(id);
+        if (!registerList.isEmpty() || user != null)
+            throw new InternalServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR, 500, "User", "The server failed to perform the operation");
     }
-
-
-
 
 
 }
