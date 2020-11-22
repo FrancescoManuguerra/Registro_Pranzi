f1a002166ca3d7b706ffffb8e30e149a198c4efe
FrancescoManuguerra <manuguerra.fr@gmail.com>
2020-11-22 10:17:44 +0100

    log added

--- RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/controller/PrenotationController.java
+++ RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/controller/PrenotationController.java
@@ -1,17 +1,25 @@
 package Elis.College.RegistroPranzi.controller;
 
-import Elis.College.RegistroPranzi.exception.model.exceptionimpl.InputParameterException;
+import Elis.College.RegistroPranzi.exception.model.Result;
+import Elis.College.RegistroPranzi.exception.model.exceptionimpl.ResponseHeaderFiller;
+import Elis.College.RegistroPranzi.model.NumberOfPrenotationResponse;
 import Elis.College.RegistroPranzi.model.Register;
-import Elis.College.RegistroPranzi.model.User;
+import Elis.College.RegistroPranzi.model.RegisterResponse;
 import Elis.College.RegistroPranzi.service.RegisterService;
+import Elis.College.RegistroPranzi.utility.RequestValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+import javax.annotation.PostConstruct;
 import java.sql.Date;
-import java.util.List;
+import java.sql.Timestamp;
+import java.util.UUID;
 
 @RestController
 public class PrenotationController {
@@ -19,36 +27,97 @@ public class PrenotationController {
     @Autowired
     RegisterService registerService;
 
+    private final Logger logger = LoggerFactory.getLogger(PrenotationController.class);
+
     //TODO doesn't work
     //get prenotations by id
+
+    //GET prenotations by userId
     @CrossOrigin
     @GetMapping(value = "/prenotations/{userId}")
-    public ResponseEntity<List<Register>> getAllByUser_id(@PathVariable(name = "userId", required = true) Long userId) throws InputParameterException {
-        System.out.println("Controller: "+ userId);
+    public ResponseEntity<RegisterResponse> getAllByUser_id(@PathVariable(name = "userId", required = true) Long userId) throws Exception {
 
-         return new ResponseEntity<>(registerService.getAllByUser_id(userId),HttpStatus.OK);
+        String logID = generateLogID("GET prenotation by userId");
+        Timestamp startTime = startLog(logID);
 
+        RequestValidator.checkParameter(userId, "userid");
+
+        RegisterResponse response = RegisterResponse.builder()
+                .result(new Result(0, "Operation completed successfully"))
+                .registerList(registerService.getAllByUser_id(userId, logID))
+                .build();
+
+        HttpHeaders headers = ResponseHeaderFiller.setHeaders(response);
+
+        endLog(logID, startTime);
+        return new ResponseEntity<>(response, headers, HttpStatus.OK);
     }
 
+    //Get all prenotations
     @CrossOrigin
-    @GetMapping(value="/prenotations")
-    public ResponseEntity<Integer> countPrenotationByDate(@RequestParam(value = "date" ,required = true) Date date){
+    @GetMapping(value = "/prenotations")
+    public ResponseEntity<NumberOfPrenotationResponse> countPrenotationByDate(@RequestParam(value = "date", required = true) Date date) throws Exception {
 
-        System.out.println("ConTROLLER: date: "+date );
+        String logID = generateLogID("GET number of prenotation by date");
+        Timestamp startTime = startLog(logID);
 
-        return new ResponseEntity<>(registerService.countPrenotationsByDate(date), HttpStatus.OK);
+        RequestValidator.checkParameter(date, "date");
+
+        NumberOfPrenotationResponse response = NumberOfPrenotationResponse.builder()
+                .result(new Result(0, "Operation completed successfully"))
+                .value(registerService.countPrenotationsByDate(date, logID))
+                .build();
+
+        HttpHeaders headers = ResponseHeaderFiller.setHeaders(response);
+
+        endLog(logID, startTime);
+        return new ResponseEntity<>(response, headers, HttpStatus.OK);
     }
 
-
-
+    //PUT prenotation on db
     @CrossOrigin
     @PutMapping(value = "/prenotations")
-    public ResponseEntity savePrenotation(@RequestBody Register register){
-        System.out.println("Controller prenotations: register.date: "+ register.getDate());
-        registerService.savePrenotation(register);
-        return new ResponseEntity(HttpStatus.OK);
+    public ResponseEntity savePrenotation(@RequestBody Register register) throws Exception {
+
+        String logID = generateLogID("POST an prenotation");
+        Timestamp startTime = startLog(logID);
+
+        RequestValidator.checkParameter(register, "prenotation");
+        RequestValidator.checkBody(register);
+
+        registerService.savePrenotation(register, logID);
+
+        Result result = new Result(0, "Operation completed successfully");
+        HttpHeaders headers = ResponseHeaderFiller.setHeaders(result);
+
+        endLog(logID, startTime);
+        return new ResponseEntity(headers, HttpStatus.OK);
     }
 
+    //LOG
+    private String generateLogID(String processName) {
 
+        String uniqueID = UUID.randomUUID().toString();
+        String logID = "[" + uniqueID + "][" + processName + "]";
+        return logID;
+    }
+
+    private Timestamp startLog(String logID) {
+        Timestamp startTime = new Timestamp(System.currentTimeMillis());
+        logger.info(logID + " Starting process at: " + startTime);
+        return startTime;
+    }
+
+    private void endLog(String logID, Timestamp startTime) {
+        Timestamp endTime = new Timestamp(System.currentTimeMillis());
+        long diff = endTime.getTime() - startTime.getTime();
+        logger.info(logID + " Ending process at: " + endTime
+                + ", duration " + diff + " ms ");
+    }
+
+    @PostConstruct
+    private void imAlive() {
+        logger.info(" Prenotation Controller started");
+    }
 
 }
--- RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/controller/UserController.java
+++ RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/controller/UserController.java
@@ -1,17 +1,27 @@
 package Elis.College.RegistroPranzi.controller;
 
+import Elis.College.RegistroPranzi.exception.model.Result;
+import Elis.College.RegistroPranzi.exception.model.exceptionimpl.ResponseHeaderFiller;
 import Elis.College.RegistroPranzi.model.LoginKey;
 import Elis.College.RegistroPranzi.model.User;
+import Elis.College.RegistroPranzi.model.UserResponse;
+import Elis.College.RegistroPranzi.model.UsersResponse;
 import Elis.College.RegistroPranzi.service.UserService;
+import Elis.College.RegistroPranzi.utility.RequestValidator;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.annotation.PostConstruct;
+import java.sql.Timestamp;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -19,46 +29,136 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    //GET ALL
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     //GET USER FOR AUTHENTICATION
     @CrossOrigin
-    @GetMapping(value="/login")
-    public ResponseEntity<User> login(@RequestParam(name = "email",required = false)String email, @RequestParam(name = "password",required = false)String password){
-        return new ResponseEntity<User>(userService.login(new LoginKey(email, DigestUtils.sha256Hex(password))), HttpStatus.OK);
+    @GetMapping(value = "/login")
+    public ResponseEntity<UserResponse> login(@RequestParam(name = "email", required = false) String email, @RequestParam(name = "password", required = false) String password) throws Exception {
 
+        String logID = generateLogID("Login");
+        Timestamp startTime = startLog(logID);
+
+        //INPUT VALIDATOR
+        RequestValidator.checkLoginParameter(email,password);
+
+        UserResponse response= UserResponse.builder()
+                .result(new Result(0,"Operation completed successfully"))
+                .user(userService.login(new LoginKey(email, DigestUtils.sha256Hex(password)),logID))
+                .build();
+        HttpHeaders headers = ResponseHeaderFiller.setHeaders(response);
+
+        endLog(logID, startTime);
+        return new ResponseEntity<>(response,headers, HttpStatus.OK);
     }
+
     //GET USER BY ID
     @CrossOrigin
-    @GetMapping(value="users/{userid}")
-    public ResponseEntity<User> getUser(@PathVariable(name = "userid") Long id){
-            return new ResponseEntity<User>(userService.getUser(id),HttpStatus.OK);
+    @GetMapping(value = "users/{userid}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable(name = "userid") Long id) throws Exception {
 
+        String logID = generateLogID("GET user by id");
+        Timestamp startTime = startLog(logID);
+
+        //INPUT VALIDATOR
+        RequestValidator.checkParameter(id, "userid");
+        RequestValidator.checkUserId(id);
+
+        UserResponse response= UserResponse.builder()
+                .result(new Result(0,"Operation completed successfully"))
+                .user(userService.getUser(id,logID))
+                .build();
+        HttpHeaders headers = ResponseHeaderFiller.setHeaders(response);
+
+        endLog(logID, startTime);
+        return new ResponseEntity<>(response,headers, HttpStatus.OK);
     }
 
     //GET USERS
     @CrossOrigin
     @GetMapping(value = "/users")
-    public ResponseEntity<List<User>> getUsers(){
-        return new ResponseEntity<List<User>>(userService.getUsers(),HttpStatus.OK);
+    public ResponseEntity<UsersResponse> getUsers() {
+
+        String logID = generateLogID("GET all users");
+        Timestamp startTime = startLog(logID);
+
+        UsersResponse response= UsersResponse.builder()
+                .result(new Result(0, "Operation completed successfully"))
+                .userList(userService.getUsers(logID))
+                .build();
+
+        HttpHeaders headers = ResponseHeaderFiller.setHeaders(response);
+
+        endLog(logID, startTime);
+        return new ResponseEntity<>(response,headers, HttpStatus.OK);
     }
+
     //SAVE USER
     @CrossOrigin
-    @PostMapping(value = "/users",produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<User> insertUser(@RequestBody(required = true) User user){
+    @PostMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserResponse> insertUser(@RequestBody(required = true) User user) throws Exception {
 
-       return new ResponseEntity<>(userService.saveUser(User.builder().name(user.getName()).surname(user.getSurname()).number(user.getNumber()).email(user.getEmail()).password(DigestUtils.sha256Hex(user.getPassword())).lable_number(user.getLable_number()).build()),HttpStatus.OK);
+        String logID = generateLogID("POST an user");
+        Timestamp startTime = startLog(logID);
 
+        RequestValidator.checkParameter(user, "User body");
+        RequestValidator.checkBody(user);
+
+        UserResponse response= UserResponse.builder()
+                .result(new Result(0, "Operation completed successfully"))
+                .user(userService.saveUser(User.builder()
+                        .name(user.getName())
+                        .surname(user.getSurname())
+                        .number(user.getNumber())
+                        .email(user.getEmail())
+                        .password(DigestUtils.sha256Hex(user.getPassword()))
+                        .lable_number(user.getLable_number())
+                        .build(),logID))
+                .build();
+        HttpHeaders headers = ResponseHeaderFiller.setHeaders(response);
+
+        endLog(logID, startTime);
+        return new ResponseEntity<>(response,headers, HttpStatus.OK);
     }
+
     //DELETE USER
     @CrossOrigin
     @DeleteMapping(value = "/users")
-    public ResponseEntity deleteUser(@RequestParam(value = "id") Long id){
-        userService.deleteUserById(id);
-        return new ResponseEntity (HttpStatus.OK);
+    public ResponseEntity<Result> deleteUser(@RequestParam(value = "id") Long id) throws Exception{
+
+        String logID = generateLogID("DELETE an user by id");
+        Timestamp startTime = startLog(logID);
+
+        RequestValidator.checkParameter(id, "userid");
+
+        userService.deleteUserById(id,logID);
+
+        endLog(logID, startTime);
+        return new ResponseEntity<>(new Result(0,"Operation completed successfully"), HttpStatus.OK);
     }
 
+    @PostConstruct
+    private void imAlive(){
+        logger.info(" Prenotation Controller started");
+    }
 
+    private String generateLogID(String processName) {
 
+        String uniqueID = UUID.randomUUID().toString();
+        String logID = "[" + uniqueID + "][" + processName + "]";
+        return logID;
+    }
 
+    private Timestamp startLog(String logID) {
+        Timestamp startTime = new Timestamp(System.currentTimeMillis());
+        logger.info(logID + " Starting process at: " + startTime);
+        return startTime;
+    }
+
+    private void endLog(String logID, Timestamp startTime) {
+        Timestamp endTime = new Timestamp(System.currentTimeMillis());
+        long diff = endTime.getTime() - startTime.getTime();
+        logger.info(logID + " Ending process at: " + endTime
+                + ", duration " + diff + " ms ");
+    }
 }
