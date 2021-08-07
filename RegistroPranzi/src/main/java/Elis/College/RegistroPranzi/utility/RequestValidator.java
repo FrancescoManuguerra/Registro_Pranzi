a95d86fcc8935007909ab549f07d977762a15ffe
FrancescoManuguerra <manuguerra.fr@gmail.com>
2021-08-07 12:19:15 +0200

    checkBody method upgrated

--- RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/utility/RequestValidator.java
+++ RegistroPranzi/src/main/java/Elis/College/RegistroPranzi/utility/RequestValidator.java
@@ -1,6 +1,5 @@
 package Elis.College.RegistroPranzi.utility;
 
-import Elis.College.RegistroPranzi.RegistroPranziApplication;
 import Elis.College.RegistroPranzi.exception.model.exceptionimpl.InputParameterException;
 import Elis.College.RegistroPranzi.model.Register;
 import Elis.College.RegistroPranzi.model.User;
@@ -8,7 +7,7 @@ import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 
-import java.util.List;
+import static Elis.College.RegistroPranzi.utility.Utilities.isNullorEmpty;
 
 public class RequestValidator {
 
@@ -44,17 +43,27 @@ public class RequestValidator {
 
         isValidEmailAddress(user.getEmail());
 
-        if (user.getName().isEmpty() || user.getNumber().isEmpty() || user.getPassword().isEmpty() || user.getSurname().isEmpty() || user.getLable_number() <= 0)
-            throw new InputParameterException(HttpStatus.BAD_REQUEST, 400, "ERROR", "The body must have all parameter");
+        if (isNullorEmpty(user.getName()))
+            throw new InputParameterException(HttpStatus.BAD_REQUEST, 400, "ERROR", "The body must have 'name' as parameter");
+        if (isNullorEmpty(user.getSurname()))
+            throw new InputParameterException(HttpStatus.BAD_REQUEST, 400, "ERROR", "The body must have 'surname' as parameter");
+        if (isNullorEmpty(user.getNumber()))
+            throw new InputParameterException(HttpStatus.BAD_REQUEST, 400, "ERROR", "The body must have 'number' as parameter");
+        if (isNullorEmpty(user.getPassword()))
+            throw new InputParameterException(HttpStatus.BAD_REQUEST, 400, "ERROR", "The body must have 'password' as parameter");
+        if (user.getLable_number() == null)
+            throw new InputParameterException(HttpStatus.BAD_REQUEST, 400, "ERROR", "The body must have 'lable_number' as parameter");
+        if (user.getLable_number() <= 0)
+            throw new InputParameterException(HttpStatus.BAD_REQUEST, 400, "ERROR", "The 'lable_number' paramenter must be greater then 0");
 
     }
 
     public static void checkBody(Register register) throws InputParameterException {
 
-        System.out.println(register.getDate()+" id:"+ register.getId()+ " dinner "+register.getDinner() +" lunch "+ register.getLunch());
+        System.out.println(register.getDate() + " id:" + register.getId() + " dinner " + register.getDinner() + " lunch " + register.getLunch());
 
         //if (register.getDate() == null || register.getId() == null || register.getDinner() == null || register.getLunch() == null )
-           // throw new InputParameterException(HttpStatus.BAD_REQUEST, 400, "ERROR", "The body must have all parameter");
+        // throw new InputParameterException(HttpStatus.BAD_REQUEST, 400, "ERROR", "The body must have all parameter");
     }
 
     //To check login parameters
